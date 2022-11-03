53aeb3d8fc3a2215b8a9483b5ba2a2b792a91393
adhillon0817 <avleendhillon@gmail.com>
2022-11-02 23:27:19 -0700

    added file startup and notes to start

---
+++ script.js
@@ -0,0 +1,11 @@
+var apiKey = "b6150b7a9c752389e1b9cd938e4b061e";
+
+//Submit the form to fetch weather information
+
+// Handle button clicks to fetch weather information
+
+// Fetch Geolocation Data (Geocoding API)
+
+
+
+// Fetch Weather Data (Onecall)
