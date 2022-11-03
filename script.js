189fff6e738d7931d559d519e2b2bb055ed5d7d0
adhillon0817 <avleendhillon@gmail.com>
2022-11-02 23:40:08 -0700

    added funtion for geolocation with api link

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ var apiKey = "b6150b7a9c752389e1b9cd938e4b061e";
 // Fetch Geolocation Data (Geocoding API)
 
 
-
 // Fetch Weather Data (Onecall)
+
+function fetchGeolocation(cityName) {
+    var request = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b6150b7a9c752389e1b9cd938e4b061e';
+
+    
+    fetch(request)
+    .then(funtion(response) {
+        return response.json();
+    })
+}
