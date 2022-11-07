94b7427e2d0e183a130eeb6c04e123cbd8eee43c
adhillon0817 <avleendhillon@gmail.com>
2022-11-07 14:27:59 -0800

    added onecallweather

--- script.js
+++ script.js
@@ -2,19 +2,51 @@ var apiKey = "b6150b7a9c752389e1b9cd938e4b061e";
 
 //Submit the form to fetch weather information
 
+    //Fetch the city name from the text <input>
+    var lat = $("#lat-field").val(); 
+    // Call the 'fetchGeolocation' and pass the city name
+
 // Handle button clicks to fetch weather information
+
+    // Get the city name from the clicked button's (event.target) data-name attribute
+
+    // Call the 'fetchGeolocation' and pass the city name 
 
 // Fetch Geolocation Data (Geocoding API)
 
-
-// Fetch Weather Data (Onecall)
-
 function fetchGeolocation(cityName) {
-    var request = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b6150b7a9c752389e1b9cd938e4b061e';
+    var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=b6150b7a9c752389e1b9cd938e4b061e`;
 
     
     fetch(request)
     .then(funtion(response) {
         return response.json();
     })
+    .then(function(data) {
+        console.log(data);
+    })
 }
+
+fetchGeolocation("Seattle");
+fetchGeolocation("Simmey Valley");
+
+
+// Fetch Weather Data (Onecall)
+
+function fetchOneCallWeather() {
+    
+    var request = `https://api.openweathermap.org/data/2.5/onecall?appid=b6150b7a9c752389e1b9cd938e4b061e&lat=${lat}.4404925&lon=${log}&units=imperial&exclude=hourly,minutely`
+
+
+    fetch(request)
+    .then(funtion(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+
+    
+}
+
+fetchOneCallWeather();
