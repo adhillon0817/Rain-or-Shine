749b3a2cf40f3ea6b2695e3ed3f03645943bde3e
adhillon0817 <avleendhillon@gmail.com>
2022-11-11 22:04:23 -0800

    added css changes

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 var apiKey = "b6150b7a9c752389e1b9cd938e4b061e";
 var selection = "";
-var date = ;
+var currentTime = moment().format("MM-DD-YYY");
+
+function fetchGeolocation(cityName) {
+    var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=b6150b7a9c752389e1b9cd938e4b061e`;
+    
+     
+    fetch(request)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+
+}
+
+function fetchOneCallWeather() {
+    
+    var  longLat = `https://api.openweathermap.org/data/2.5/onecall?appid=b6150b7a9c752389e1b9cd938e4b061e&lat=${lat}.4404925&lon=${log}&units=imperial&exclude=hourly,minutely`
+
+
+    fetch(longLat)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+
+    
+}
+
+fetchOneCallWeather();
