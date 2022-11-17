9a91c03c0b7cdad403b35027410b8603a5f98afb
adhillon0817 <avleendhillon@gmail.com>
2022-11-17 10:20:12 -0800

    added to css properties

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var apiKey = "b6150b7a9c752389e1b9cd938e4b061e";
+var apiKey = "0f003ceaa60fba131d9eb1c9a697784c";
 var cityName = "";
-var search = JSON.parse(localStorage.getItem("city", JSON.stringify(selection)));
-var currentTime = moment().format("MM-DD-YYY");
+// var search = JSON.parse(localStorage.getItem("city", JSON.stringify(selection)));
+// var currentTime = moment().format("MM-DD-YYY");
 
-function renderLastRegistered();
+// function renderLastRegistered();
 
 var citySelection = JSON.parse
 
 function fetchGeolocation(cityName) {
-    var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=b6150b7a9c752389e1b9cd938e4b061e`;
+    var request = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=0f003ceaa60fba131d9eb1c9a697784c
+    `;
     
      
     fetch(request)
@@ -17,13 +18,17 @@ function fetchGeolocation(cityName) {
     })
     .then(function(data) {
         console.log(data);
+        //need to get the lat and long fromd ata object
+        //then call fetchonecallweiather and pass lat and long 
     })
+
 
 }
 
-function fetchOneCallWeather() {
+function fetchOneCallWeather(lat, lon) {
     
-    var  longLat = `https://api.openweathermap.org/data/2.5/onecall?appid=b6150b7a9c752389e1b9cd938e4b061e&lat=${lat}.4404925&lon=${log}&units=imperial&exclude=hourly,minutely`
+    
+    var  longLat = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=b6150b7a9c752389e1b9cd938e4b061e`
 
 
     fetch(longLat)
@@ -37,4 +42,4 @@ function fetchOneCallWeather() {
     
 }
 
-fetchOneCallWeather();
+fetchGeolocation();
