085abdab8bd738d3a264ba53de27905b975ab648
adhillon0817 <avleendhillon@gmail.com>
2022-11-17 12:54:04 -0800

    set up getApi

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var cityfindEl = document.getElementsById("searchBox");
+var cityfindEl = document.getElementById("searchBox");
 var findEl = document.getElementById("find"); 
 
-var cityNameEl = document.getElementById("city-name");
+var cityNameEl = document.getElementById("cityName");
 var currentDateEl = document.getElementById("date");
 var currentTempEl = document.getElementById("temperature");
 var currentWindEl = document.getElementById("wind");
 var currentHumidityEl = document.getElementById("humidity");
+
 
 var today = moment().format("MM/DD/YYYY");
 
@@ -43,34 +44,36 @@ var dayFiveHumidityEl = document.getElementById("humidityfive");
 
 
 // var search = JSON.parse(localStorage.getItem("city", JSON.stringify(selection)));
-// var currentTime = moment().format("MM-DD-YYY");
+
 
 // function renderLastRegistered();
 
 
 
-function fetchGeolocation(cityName) {
-    var request = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=0f003ceaa60fba131d9eb1c9a697784c
-    `;
+function getApi(cityName) {
+    var request = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=0f003ceaa60fba131d9eb1c9a697784c`;
     
      
     fetch(request)
     .then(function(response) {
         return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        //need to get the lat and long fromd ata object
-        //then call fetchonecallweiather and pass lat and long 
-    })
+    })  
 
+    .then(function(data) {
+        displayNameEl.innerHTML=data.name;
+        currentDateEl.innerHTML=today;
+        currentTempEl.innerHTML= "Temperature: " + data.main.temp + "°F";
+        currentWindEl.innerHTML= "Wind: " + data.wind.speed + "mph";
+        currentHumidityEl.innerHTML= "Humidity: " + data.main.humidity + "%";
+        
+    })
 
 }
 
 function fetchOneCallWeather(lat, lon) {
     
     
-    var  longLat = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=0f003ceaa60fba131d9eb1c9a697784c`
+    var longLat = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=0f003ceaa60fba131d9eb1c9a697784c`
 
 
     fetch(longLat)
